aea06dfcc2e6ddd410a59bb6760d4060b8f7563b
BFSpecialProjects <43890684+BFSpecialProjects@users.noreply.github.com>
2019-02-12 21:26:50 -0500

    Update twilio.py

--- twilio.py
+++ twilio.py
@@ -3,8 +3,8 @@
 # Run following code (VS Code or PowerShell both work well if you dont have an IDE)
 from twilio.rest import Client
 # Create trial account and replace placeholder text
-client = Client("Account SID here", "Auth Token here")
+client = Client("ACb5ea38bc4c703627eef8b374dd1d204e", "9e29b19d23fe9777aa4d23d75966cec3")
 # fill in to(verified nums only during trial), to (the number you chose) and body(message)
-client.messages.create(to="enter to number (verified numbers only during trial period",
-                       from_="+16172508341",
+client.messages.create(to="enter to number (verified numbers only during trial period)",
+                       from_="+19199483074",
                        body="Message Here")
