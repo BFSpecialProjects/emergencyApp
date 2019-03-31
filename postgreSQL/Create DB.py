4fce3cd4f9b5251e8365563080b5eb6a3e09c206
Kanv Khurana <12547828+kanvk@users.noreply.github.com>
2019-03-31 13:53:07 -0400

    Update Create DB.py

--- postgreSQL/Create DB.py
+++ postgreSQL/Create DB.py
@@ -1,21 +1,38 @@
 import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres port=5432 user=postgres password=nicetry")
-cur = conn.cursor()
+from psycopg2 import Error
 
-# cur.execute(command)
-cur.execute("""
-    CREATE TABLE Test(
+try:
+    connection = psycopg2.connect(host="localhost",
+                                  database="postgres",
+                                  port="5432",
+                                  user="postgres",
+                                  password="pass")
+    cursor = connection.cursor()
+    print(connection.get_dsn_parameters(), "\n")
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+    cursor.execute("""
+    CREATE TABLE Arrays(
     ID_Number integer PRIMARY KEY,
     ArrayA integer,
     ArrayB integer)
 """)
-conn.commit()
-
-cur.execute("""
+    print("Table Arrays created successfully")
+    cursor.execute("""
    CREATE TABLE Teachers(
     ID_Number integer PRIMARY KEY,
     Name text,
     Email text,
     Phone_Number text)
     """)
-conn.commit()
+    connection.commit()
+    print("Table Teachers created successfully")
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
