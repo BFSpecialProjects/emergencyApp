df8102b28a218302b49167b6acf5fda02ab1d67b
BFSpecialProjects <43890684+BFSpecialProjects@users.noreply.github.com>
2019-04-11 09:15:09 -0400

    Update Translate.py

--- Translate.py
+++ Translate.py
@@ -1,6 +1,15 @@
-# pip3 install py_translator
+import boto3 # AWS
+from py_translator import Translator # Google Translate 
 
-from py_translator import Translator
-
+# Google Translate
 text = Translator().translate(text='Hello World', dest='es').text
 print(text)
+
+# AWS
+translate = boto3.client(service_name='translate', region_name='region', use_ssl=True)
+
+result = translate.translate_text(Text="Hello, World", 
+            SourceLanguageCode="en", TargetLanguageCode="de")
+print('TranslatedText: ' + result.get('TranslatedText'))
+print('SourceLanguageCode: ' + result.get('SourceLanguageCode'))
+print('TargetLanguageCode: ' + result.get('TargetLanguageCode'))
